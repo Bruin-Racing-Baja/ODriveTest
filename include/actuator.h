c3cf9c5c0d43ef1edff213bc9d8e33318be1388b
Gautier404 <and.jgautier@gmail.com>
2021-11-11 22:18:14 -0800

    created actuator class to handle everything to do with actuator controls

---
+++ include/actuator.h
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+#ifndef actuator_h
+#define actuator_h
+
+class Actuator{
+    public:
+    Actuator(HardwareSerial& serial, const int egTooth, const int gbTooth, const int inboundHall, const int outboundHall);
+    void initialize();
+    void control_function();
+    int get_status_code();
+
+    private:
+    HardwareSerial& serial_;
+
+    // Functions that command ODrive
+    bool run_state(int axis, int requested_state, bool wait_for_idle, float timeout);
+    void set_velocity(float velocity);
+
+    // Functions that get information from Odrive
+    int32_t read_int();
+    String read_string();
+    String dump_errors();
+
+    // member variables for sensor pins
+    int m_egTooth;
+    int m_gbTooth;
+    int m_inboundHall;
+    int m_outboundHall;
+
+    // Rando Constants
+    const int m_motor_number = 0;
+    const int status_code = 2;
+
+    bool hasRun;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <SoftwareSerial.h>
 #include <ODriveArduino.h>
 
+#include <actuator.h>
 // Printing with stream operator helper functions
-template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
-template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
+// template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
+// template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
 
 
 ////////////////////////////////
@@ -23,98 +24,106 @@ template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(a
 // pin 0: RX - connect to ODrive TX
 // pin 1: TX - connect to ODrive RX
 // See https://www.pjrc.com/teensy/td_uart.html for other options on Teensy
-HardwareSerial& odrive_serial = Serial1;
+
+//HardwareSerial& odrive_serial = Serial1;
+
 int input_pot = 14;
 
 
 // ODrive object
-ODriveArduino odrive(odrive_serial);
+//ODriveArduino odrive(Serial1);
 //serial_(Serial1) {}
+
+Actuator actuator(Serial1, 0, 0, 0, 0);
+
 void setup() {
-  // ODrive uses 115200 baud
-  odrive_serial.begin(115200);
+  actuator.initialize();
 
-  // Serial to PC
-  Serial.begin(115200);
-  while (!Serial) ; // wait for Arduino Serial Monitor to open
+  // // ODrive uses 115200 baud
+  // odrive_serial.begin(115200);
 
-  Serial.println("ODriveArduino");
-  Serial.println("Setting parameters...");
+  // // Serial to PC
+  // Serial.begin(115200);
+  // while (!Serial) ; // wait for Arduino Serial Monitor to open
 
-  // In this example we set the same parameters to both motors.
-  // You can of course set them different if you want.
-  // See the documentation or play around in odrivetool to see the available parameters
+  // Serial.println("ODriveArduino");
+  // Serial.println("Setting parameters...");
 
-  Serial.println("Ready!");
-  Serial.println("Send the character '0' or '1' to calibrate respective motor (you must do this before you can command movement)");
-  Serial.println("Send the character 's' to exectue test move");
-  Serial.println("Send the character 'b' to read bus voltage");
-  Serial.println("Send the character 'p' to read motor positions in a 10s loop");
-  Serial.println("Send the character 'q' for potentiometer velocity control");
-  odrive.run_state(0, 1, true);
+  // // In this example we set the same parameters to both motors.
+  // // You can of course set them different if you want.
+  // // See the documentation or play around in odrivetool to see the available parameters
+
+  // Serial.println("Ready!");
+  // Serial.println("Send the character '0' or '1' to calibrate respective motor (you must do this before you can command movement)");
+  // Serial.println("Send the character 's' to exectue test move");
+  // Serial.println("Send the character 'b' to read bus voltage");
+  // Serial.println("Send the character 'p' to read motor positions in a 10s loop");
+  // Serial.println("Send the character 'q' for potentiometer velocity control");
+  // odrive.run_state(0, 1, true);
 }
 
 void loop() {
-  if (Serial.available()) {
-    char c = Serial.read();
+  actuator.control_function();
+  // if (Serial.available()) {
+  //   char c = Serial.read();
 
-    // Run calibration sequence
-    if (c == '0' || c == '1') {
-      int motornum = c-'0';
-      int requested_state;
+  //   // Run calibration sequence
+  //   if (c == '0' || c == '1') {
+  //     int motornum = c-'0';
+  //     int requested_state;
 
-      //odrive.EncoderConfig(c);
-      requested_state = 3;
-      Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
-      if(!odrive.run_state(motornum, requested_state, true)) return;
-      Serial<<odrive.DumpErrors()<<"\n";
-    }
+  //     //odrive.EncoderConfig(c);
+  //     requested_state = 3;
+  //     Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+  //     if(!odrive.run_state(motornum, requested_state, true)) return;
+  //     Serial<<odrive.DumpErrors()<<"\n";
+  //   }
 
-    // Follow Potentiometer for Velocity
-    if (c == 'q') {
-      float goalVel = 0;
-      odrive.run_state(0, 8, false, 10);
-      for(float i = 0; i <100; i++) {
-        goalVel = map(analogRead(input_pot), 0, 1023, 0, 20);
-        odrive.SetVelocity(0, goalVel);
-        delay(100);
-      }
-      odrive.SetVelocity(0, 0);
-      odrive.run_state(0, 1, false, 10);
-    }
+  //   // Follow Potentiometer for Velocity
+  //   if (c == 'q') {
+  //     float goalVel = 0;
+  //     odrive.run_state(0, 8, false, 10);
+  //     for(float i = 0; i <100; i++) {
+  //       goalVel = map(analogRead(input_pot), 0, 1023, 0, 20);
+  //       odrive.SetVelocity(0, goalVel);
+  //       delay(100);
+  //     }
+  //     odrive.SetVelocity(0, 0);
+  //     odrive.run_state(0, 1, false, 10);
+  //   }
 
-    // Sinusoidal test move
-    if (c == 's') {
-        Serial.println("Executing test spin");
-        odrive.run_state(0, 8, false, 10);
+  //   // Sinusoidal test move
+  //   if (c == 's') {
+  //       Serial.println("Executing test spin");
+  //       odrive.run_state(0, 8, false, 10);
 
-        for(float i = 5; i<20; i += 1) {
-            Serial<<"Velocity: "<<i<<"\n";
-            odrive.SetVelocity(0, i);
-            delay(1000);
-        }
-        odrive.SetVelocity(0, 0);
-        odrive.run_state(0, 1, false, 10);
+  //       for(float i = 5; i<20; i += 1) {
+  //           Serial<<"Velocity: "<<i<<"\n";
+  //           odrive.SetVelocity(0, i);
+  //           delay(1000);
+  //       }
+  //       odrive.SetVelocity(0, 0);
+  //       odrive.run_state(0, 1, false, 10);
 
-        Serial<<odrive.DumpErrors()<<"\n";
-    }
+  //       Serial<<odrive.DumpErrors()<<"\n";
+  //   }
 
-    // Read bus voltage
-    if (c == 'b') {
-      odrive_serial << "r vbus_voltage\n";
-      Serial << "Vbus voltage: " << odrive.readFloat() << '\n';
-    }
+  //   // Read bus voltage
+  //   if (c == 'b') {
+  //     odrive_serial << "r vbus_voltage\n";
+  //     Serial << "Vbus voltage: " << odrive.readFloat() << '\n';
+  //   }
 
-    // print motor positions in a 10s loop
-    if (c == 'p') {
-      static const unsigned long duration = 10000;
-      unsigned long start = millis();
-      while(millis() - start < duration) {
-        for (int motor = 0; motor < 2; ++motor) {
-          Serial << odrive.GetVelocity(motor) << '\t';
-        }
-        Serial << '\n';
-      }
-    }
-  }
+  //   // print motor positions in a 10s loop
+  //   if (c == 'p') {
+  //     static const unsigned long duration = 10000;
+  //     unsigned long start = millis();
+  //     while(millis() - start < duration) {
+  //       for (int motor = 0; motor < 2; ++motor) {
+  //         Serial << odrive.GetVelocity(motor) << '\t';
+  //       }
+  //       Serial << '\n';
+  //     }
+  //   }
+  // }
 }
